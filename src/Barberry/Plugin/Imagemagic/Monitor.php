a2f57840ca58ef1bcbc1e078e0582ba8e727e23f
Andrew Kevich <kevich@xiag.ch>
2012-10-23 10:45:25 +0700

    + added real monitor functionality (+tests)

--- src/Barberry/Plugin/Imagemagic/Monitor.php
+++ src/Barberry/Plugin/Imagemagic/Monitor.php
@@ -4,18 +4,49 @@ use Barberry\Plugin;
 
 class Monitor implements Plugin\InterfaceMonitor
 {
+    private $tempDirectory;
+
+    private $dependencies = array(
+        'convert' => 'Please install imagemagic (http://www.imagemagick.org)'
+    );
+
     public function __construct($tmpDir)
     {
+        $this->tempDirectory = $tmpDir;
+    }
 
+    public function dependencies()
+    {
+        return $this->dependencies;
     }
 
     public function reportUnmetDependencies()
     {
-        return array();
+        $errors = array();
+        foreach ($this->dependencies() as $command => $message) {
+            $answer = $this->reportUnixCommand($command, $message);
+            if (!is_null($answer)) {
+                $errors[] = $answer;
+            }
+        }
+        return $errors;
     }
 
     public function reportMalfunction()
     {
-        return array();
+        $answer = $this->reportWritableDirectory($this->tempDirectory);
+        return (!is_null($answer)) ? array($answer) : array();
+    }
+
+//-------------------------------------------------------------------------
+
+    private function reportWritableDirectory($directory)
+    {
+        return (!is_writeable($directory)) ? 'ERROR: Plugin imagemagic temporary directory is not writeable.' : null;
+    }
+
+    private function reportUnixCommand($command, $messageIfMissing)
+    {
+        return preg_match('/^\/\w+/', exec("which $command")) ? null : "MISSING - $messageIfMissing\n\n";
     }
 }
